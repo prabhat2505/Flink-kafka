454ad01d11e92c34a64c9ea692fd88151fb7c49f
Prabhat Rana <prabhat.rana828@gmail.com>
2023-11-09 15:43:17 +0530

    first commit

---
+++ src/main/java/com/example/springbootflink/SpringbootFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootflink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFlinkApplication.class, args);
+	}
+
+}
